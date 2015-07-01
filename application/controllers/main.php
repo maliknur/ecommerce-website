6b5eba9ec0f8c87a4f0d4334b3622bc7916f03a3
Chris Tran <christopher.huytran@gmail.com>
2015-07-01 00:27:11 -0700

    created partial views for header and navbar

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -3,23 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
-	
 	public function __construct()
 	{
 		parent::__construct();
-		// $this->output->enable_profiler();
-	
+		// $this->output->enable_profiler();	
 	}
-
-    // main page
 
 	public function index()
 	{	
 	   $this->load->view('index');
 	}
-
-
-
-
-
 }
---
+++ application/views/partials/navbar.php
@@ -0,0 +1,17 @@
+<nav>
+	<div class="nav-wrapper">
+		<a href="#" class="brand-logo center">Mass Buy</a>
+		<ul id="nav-mobile" class="left hide-on-med-and-down">
+			<li><a href="sass.html">Sass</a></li>
+			<li><a href="badges.html">Components</a></li>
+			<li><a href="collapsible.html">JavaScript</a></li>
+		</ul>
+		<form class="right">
+			<div class="input-field">
+				<input id="search" type="search" required>
+				<label for="search"><i class="material-icons">search</i></label>
+				<!-- <i class="material-icons">close</i> -->
+			</div>
+		</form>
+	</div>
+</nav>
