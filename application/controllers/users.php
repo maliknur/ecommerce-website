0784addf7bad20c8cf8a15b00cb8632e718251f8
maliknur <malik.dba.pro@gmail.com>
2015-06-30 18:00:19 -0700

    initialize project

---
+++ application/controllers/users.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->output->enable_profiler();
+	
+	}
+
+    // main page
+
+	public function index()
+	{	
+	   $this->load->view('index');
+	}
+
+
+
+
+
+}
---
+++ application/views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<link rel="stylesheet" href="assets/style.css">
+	<meta charset="UTF-8">
+	
+	<title></title>
+</head>
+<body>
+
+
+
+
+
+</body>
+</html>
